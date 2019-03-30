b652790cbbe8fb93eb0a909d978fd35058743de5
Dion Pettaway <dionp@fullsail.edu>
2019-03-30 18:25:45 -0400

    Okay so the useState makes sense and is very straight forward, and useEffect so far seems pretty straightforward as well

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello
-      </div>
-    );
-  }
+export const app = () => {
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    axios.get(`http://hn.algolia.com/api/v1/search?query=visual+studio+code`)
+      .then(response => {
+        console.log(response.data)
+        setResults(response.data.hits);
+      })
+  }, [])
+
+  return ( 
+    <div>
+      {results.map(result => (
+        <li key={result.objectID}>
+          <a href={result.url}>{result.title}</a>
+        </li>
+      ))}
+    </div>
+  );
 }
-
-export default App;
+ 
+export default app;
